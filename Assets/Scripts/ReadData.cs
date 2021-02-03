08203087aec4a11db007ee3c7b073bd59daeae38
Evija Cibuļska <evija.cibulska@gmail.com>
2021-02-03 19:07:02 +0200

    Read data from file

---
+++ Assets/Scripts/ReadData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadData : MonoBehaviour
+{
+    public TextAsset dictionaryTextFile;
+    private string theWholeFileAsOneLongString;
+    private List<string> eachLine;
+
+     public void datafile_read()
+    {
+        
+    theWholeFileAsOneLongString = dictionaryTextFile.text;
+     
+     eachLine = new List<string>();
+     eachLine.AddRange(
+                 theWholeFileAsOneLongString.Split("\n"[0]) );
+     
+     Debug.Log(eachLine[0]);
+     Debug.Log(eachLine[1]);
+     Debug.Log(eachLine[2]);
+     Debug.Log(eachLine[4]);
+    }   
+}
